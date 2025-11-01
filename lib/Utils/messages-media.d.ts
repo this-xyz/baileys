03fe54087fda0721fcc0840adf57954b6575d795
Cyhper <soldierrboy445@gmail.com>
2025-11-01 12:34:38 -0400

    Add files via upload

--- lib/Utils/messages-media.d.ts
+++ lib/Utils/messages-media.d.ts
@@ -33,11 +33,7 @@ export declare function getAudioDuration(buffer: Buffer | string | Readable): Pr
 /**
   referenced from and modifying https://github.com/wppconnect-team/wa-js/blob/main/src/chat/functions/prepareAudioWaveform.ts
  */
-export declare function getAudioWaveform(
-  buffer: Buffer | string | Readable,
-  logger?: ILogger
-): Promise<Uint8Array | undefined>;
-
+export declare function getAudioWaveform(buffer: Buffer | string | Readable, logger?: ILogger): Promise<Uint8Array<ArrayBuffer> | undefined>;
 export declare const toReadable: (buffer: Buffer) => Readable;
 export declare const toBuffer: (stream: Readable) => Promise<Buffer<ArrayBuffer>>;
 export declare const getStream: (item: WAMediaUpload, opts?: AxiosRequestConfig) => Promise<{
